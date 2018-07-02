a407c510c709d1126b6d1111555948e281a60323
10747182724@qq.com <1074182724@qq.com>
2018-07-02 21:32:47 +0800

    Initial commit

---
+++ js/haha.js
@@ -0,0 +1,20 @@
+function shang() {
+    window.location.href = "http://cbox.cctv.com/phone/video/index.shtml?guid=a5e2203ea4b64ab0af1ba98eeaabec20";
+}
+
+function xia() {
+    window.location.href = "http://cbox.cctv.com/phone/video/index.shtml?guid=e3ef314d9b8c417693d8c084681aea28";
+}
+
+function pfzl() {
+    document.getElementById('div4').style.display = 'none';
+    document.getElementById('div3').style.display = 'block';
+
+    window.location.hash = "#div3";
+}
+
+function yanhuo() {
+    document.getElementById('div3').style.display = 'none';
+    document.getElementById('div4').style.display = 'block';
+    window.location.hash = "#4";
+}
